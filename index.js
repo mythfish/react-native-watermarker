b7657d63f7849f75560a21993e38e02cd3553da2
mythfish <mythfish@gmail.com>
2017-02-05 01:17:03 +0800

    Fix params

--- index.js
+++ index.js
@@ -3,31 +3,24 @@ import { NativeModules } from 'react-native';
 
 const { RNWatermarker } = NativeModules;
 
-export default RNWatermarker;
+const DEFAULT_OPTIONS = {
+  color: 'white',
+  x: 0,
+  y: 0,
+  size: 12,
+  underline: false,
+  alpha: 255
+};
 
-
-// import { NativeModules } from 'react-native';
-//
-// const DEFAULT_OPTIONS = {
-//   color: 'white',
-//   x: 0,
-//   y: 0,
-//   size: 12,
-//   underline: false,
-//   alpha: 255
-// };
-//
-// const RNWatermarker = Object.assign(
-//   NativeModules.RNWatermarker,
-//   {
-//     text: function text(options, callback) {
-//       if (typeof options === 'function') {
-//         callback = options;
-//         options = {};
-//       }
-//       return NativeModules.RNWatermarker.text(Object.assign(DEFAULT_OPTIONS, options), callback)
-//     }
-//   }
-// )
-//
-// module.exports = RNWatermarker
+export default {
+  ...RNWatermarker,
+  text: function text(path, watermark, options, callback) {
+    if (typeof options === 'function') {
+      callback = options;
+      options = DEFAULT_OPTIONS;
+    } else {
+      options = Object.assign({}, DEFAULT_OPTIONS, options)
+    }
+    return RNWatermarker.text(path, watermark, options, callback)
+  }
+}
