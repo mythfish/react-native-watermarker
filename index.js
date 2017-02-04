a9d21a717f57ba3e7aade03b9cd8a1fab52f0c69
mythfish <mythfish@gmail.com>
2017-02-05 00:23:12 +0800

    Test

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ import { NativeModules } from 'react-native';
 
 const { RNWatermarker } = NativeModules;
 
-const DEFAULT_OPTIONS = {
-  color: 'white',
-  x: 0,
-  y: 0,
-  size: 12,
-  underline: false,
-  alpha: 255
-};
+export default RNWatermarker;
 
-export default {
-  ...RNWatermarker,
-  text: function text(options, callback) {
-    if (typeof options === 'function') {
-      callback = options;
-      options = {};
-    }
-    return RNWatermarker.text({...DEFAULT_OPTIONS, ...options}, callback)
-  }
-}
+
+// import { NativeModules } from 'react-native';
+//
+// const DEFAULT_OPTIONS = {
+//   color: 'white',
+//   x: 0,
+//   y: 0,
+//   size: 12,
+//   underline: false,
+//   alpha: 255
+// };
+//
+// const RNWatermarker = Object.assign(
+//   NativeModules.RNWatermarker,
+//   {
+//     text: function text(options, callback) {
+//       if (typeof options === 'function') {
+//         callback = options;
+//         options = {};
+//       }
+//       return NativeModules.RNWatermarker.text(Object.assign(DEFAULT_OPTIONS, options), callback)
+//     }
+//   }
+// )
+//
+// module.exports = RNWatermarker
